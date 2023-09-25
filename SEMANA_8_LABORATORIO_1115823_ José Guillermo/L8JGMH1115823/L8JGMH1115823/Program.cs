bda5b6885c06bc608659a0666fd6a175099d1d8c
guillerm002 <142232718+guillerm002@users.noreply.github.com>
2023-09-25 10:01:50 -0600

    Add files via upload

---
+++ SEMANA_8_LABORATORIO_1115823_ José Guillermo/L8JGMH1115823/L8JGMH1115823/Program.cs
@@ -0,0 +1,42 @@
+﻿class Prgram
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Laboratorio semana 8");
+
+        Console.WriteLine("\nIngrese un numero ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int A = 0,B = 1, C = 0, I = 2;
+        string resultado = "";
+
+        if (N > 0)
+        {
+            // CONVERTIR VARIABLE A (A TEXTO)
+            resultado = A.ToString();
+            if (N < 1)
+            {
+                resultado += B.ToString();
+                while (I < N) ;
+                {
+                    C = A + B;
+
+                    resultado += C.ToString();
+                    A = B;
+                    B = C;
+                    I++;
+                }
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+
+        }
+      
+       
+    }
+}
