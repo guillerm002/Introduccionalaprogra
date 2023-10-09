8c4414fbaaaeb2161f95de5e18c9da8571e28a1a
guillerm002 <142232718+guillerm002@users.noreply.github.com>
2023-10-09 10:16:50 -0600

    Add files via upload

---
+++ SEMANA_10_LABORATORI_1115823_José Guillermo Morales/L10_JGMH_1115823/L10_JGMH_1115823/Program.cs
@@ -0,0 +1,41 @@
+﻿//clase principal
+using System.Text.RegularExpressions;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int numero;
+        Automovil automovil_1 = new Automovil();
+        Console.WriteLine("Modelo:" + automovil_1.modelo);
+        Console.WriteLine("Precico:" + automovil_1.precio);
+        Console.WriteLine("Marca:" + automovil_1.marca);
+        Console.WriteLine("disponible:" + automovil_1.disponible);
+        Console.WriteLine("tipoCambioDolar:" + automovil_1.tipoCambioDolar);
+        Console.WriteLine("descuentoAplicado:" + automovil_1.descuentoAplicado);
+
+    }
+    //creacion clase Automovil
+    public class Automovil
+    {
+        public int modelo { get; set; }
+        public double precio { get; set; }
+        public string marca { get; set; }
+        public bool disponible { get; set; }
+        public double tipoCambioDolar { get; set; }
+        public double descuentoAplicado { get; set; }
+
+
+        public Automovil()
+        {
+            this.modelo = 2019;
+            this.precio = 10000.00;
+            this.marca = "";
+            this.disponible = false;
+            this.tipoCambioDolar = 7.50;
+            this.descuentoAplicado = 0.00;
+        }
+        // constructor 
+
+    }
+}
